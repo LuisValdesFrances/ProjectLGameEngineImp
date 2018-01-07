4015207081220fb46e8384386f38171ef596b5d5
LUISPC\Luis <triskel67@hotmail.com>
2018-01-07 14:36:33 +0100

    Assets para varias resoluciones

--- files/Recursos Tiled/Ejecutable/BuildLevels.bat
+++ files/Recursos Tiled/Ejecutable/BuildLevels.bat
@@ -2,22 +2,65 @@
 del *.map
 
 
-java -jar LVTiledParser.jar level_1
+java -jar LVTiledParser.jar level_1_hhdpi
 
 cd "../../../files/Recursos tiled/Ejecutable/levels"
 
-move *.map ../../../../src/bin/levels
+move *.map ../../../../src/bin/levels/hhdpi
 
 cd "../../../../files/Recursos tiled/Ejecutable"
 
 rmdir /s /q levels
 
 
-java -jar LVTiledParser.jar level_rock_test
+java -jar LVTiledParser.jar level_rock_test_hhdpi
+
+cd "../../../files/Recursos tiled/Ejecutable/levels
+
+move *.map ../../../../src/bin/levels/hhdpi
+
+cd "../../../../files/Recursos tiled/Ejecutable"
+
+rmdir /s /q levels
+
+
+java -jar LVTiledParser.jar level_1_hdpi
 
 cd "../../../files/Recursos tiled/Ejecutable/levels"
 
-move *.map ../../../../src/bin/levels
+move *.map ../../../../src/bin/levels/hdpi
+
+cd "../../../../files/Recursos tiled/Ejecutable"
+
+rmdir /s /q levels
+
+
+java -jar LVTiledParser.jar level_rock_test_hdpi
+
+cd "../../../files/Recursos tiled/Ejecutable/levels
+
+move *.map ../../../../src/bin/levels/hdpi
+
+cd "../../../../files/Recursos tiled/Ejecutable"
+
+rmdir /s /q levels
+
+java -jar LVTiledParser.jar level_1_mdpi
+
+cd "../../../files/Recursos tiled/Ejecutable/levels"
+
+move *.map ../../../../src/bin/levels/mdpi
+
+cd "../../../../files/Recursos tiled/Ejecutable"
+
+rmdir /s /q levels
+
+
+java -jar LVTiledParser.jar level_rock_test_mdpi
+
+cd "../../../files/Recursos tiled/Ejecutable/levels
+
+move *.map ../../../../src/bin/levels/mdpi
 
 cd "../../../../files/Recursos tiled/Ejecutable"
 
